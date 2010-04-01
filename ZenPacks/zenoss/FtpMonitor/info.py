3a667f43bf58ce432dfc5deb985ad4700f625480
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-04-01 18:07:05 +0000

    added info/interface reviewed by JPL
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.FtpMonitor@31743 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/FtpMonitor/info.py
@@ -0,0 +1,50 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from Products.Zuul.infos import ProxyProperty
+from Products.Zuul.infos.template import RRDDataSourceInfo
+from ZenPacks.zenoss.FtpMonitor.interfaces import IFtpMonitorDataSourceInfo
+from ZenPacks.zenoss.FtpMonitor.datasources.FtpMonitorDataSource import FtpMonitorDataSource
+
+def ftpMonitorStatesVocabulary(context):
+    return SimpleVocabulary.fromValues(FtpMonitorDataSource.states)
+
+
+class FtpMonitorDataSourceInfo(RRDDataSourceInfo):
+    implements(IFtpMonitorDataSourceInfo)
+    timeout = ProxyProperty('timeout')
+    cycletime = ProxyProperty('cycletime')
+    hostname = ProxyProperty('hostname')
+    port = ProxyProperty('port')
+    sendString = ProxyProperty('sendString')
+    expectString = ProxyProperty('expectString')
+    quitString = ProxyProperty('quitString')
+    refuse = ProxyProperty('refuse')
+    mismatch = ProxyProperty('mismatch')
+    maxBytes = ProxyProperty('maxBytes')
+    delay = ProxyProperty('delay')
+    certificate = ProxyProperty('certificate')
+    useSSL = ProxyProperty('useSSL')
+    warning = ProxyProperty('warning')
+    critical = ProxyProperty('critical')
+    
+    @property
+    def testable(self):
+        """
+        We can NOT test this datsource against a specific device
+        """
+        return False
+    
+
+
